ca4eeddc447df05b41e5a19f5871b2981d07d893
hye8758 <hye8758@naver.com>
2024-05-29 15:30:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-print('helloo')
+import streamlit as st
+
+def get_conversation_chain(_db, _model, user_question, _press_release_info):
+    # Search the DB.
+    #st.write(user_question)
+
+    qestion_first = user_question.strip()[0]
+    if qestion_first == '@':
+        st.write("@표시에 따라 학생이라 가정하고 답변드리겠습니다.")
+        
+    elif qestion_first == '#':
+        st.write("#표시에 따라 보도자료가 아닌 일반적인 내용을 토대로 답변드리겠습니다.")
+        
+    else :
+        st.write("보도자료가 아닌 일반적인 내용을 토대로 답변을 원하시면 질문 앞에 #를 붙여주세요.")
+          
+def start(_db, _model, _press_release_info):
+    #st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    user_question = st.text_input("질의사항 입력", placeholder="여기에 입력해 주세요(입력 후 엔터)")
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    
+    if user_question == None:
+        st.write("아직 질문 내용이 없습니다")
+    else:
+        pass
+        #st.write(user_question)
+        
+    if user_question:
+        st.session_state.conversation = get_conversation_chain(_db, _model, user_question, _press_release_info)
+
+
+if __name__ == "__main__":   
+    start(_db, _model, _press_release_info)
